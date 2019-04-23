bf246dd116a3bcdb836b56aed32cf67f4584f773
noah <nomaxx117@gmail.com>
2019-04-23 12:48:26 -0500

    Working on constraints

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-pub enum WorldTileKind {
-    Pit,
-    Obstruction,
+use nalgebra::base::{Matrix, VecStorage};
+use nalgebra::base::dimension::Dynamic;
+use nalgebra::Dim;
 
+pub struct Grid<R, C> where R: Dim, C: Dim {
+    pub terminals: Matrix<bool, R, C, VecStorage<bool, R, C>>,
+    pub values: Matrix<f32, R, C, VecStorage<f32, R, C>>,
+    pub mutables: Matrix<bool, R, C, VecStorage<bool, R, C>>,
+    pub traversables: Matrix<bool, R, C, VecStorage<bool, R, C>>,
+}
+
+impl <R, C> Grid<R, C> where R: Dim, C: Dim {
+    pub fn constrain(&self) -> Matrix<f32, R, C, VecStorage<f32, R, C>> {
+        let mut constrained = self.values.clone();
+        constrained
+    }
 }
 
 fn main() {
