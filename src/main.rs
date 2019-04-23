656afa1adb58bd5ee4e41e63e6eab6eb92ffaafc
noah <nomaxx117@gmail.com>
2019-04-23 17:49:40 -0500

    Resolved generics

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use nalgebra::base::{Matrix, VecStorage};
-use nalgebra::base::dimension::Dynamic;
-use nalgebra::Dim;
+use nalgebra::base::{Matrix, VecStorage, MatrixMN};
+use nalgebra::base::dimension::{Dynamic, DimName};
+use nalgebra::{Dim, DefaultAllocator};
+use nalgebra::base::allocator::Allocator;
 
-pub struct Grid<R, C> where R: Dim, C: Dim {
-    pub terminals: Matrix<bool, R, C, VecStorage<bool, R, C>>,
-    pub values: Matrix<f32, R, C, VecStorage<f32, R, C>>,
-    pub mutables: Matrix<bool, R, C, VecStorage<bool, R, C>>,
-    pub traversables: Matrix<bool, R, C, VecStorage<bool, R, C>>,
+pub struct Grid<R, C> where R: Dim + DimName, C: Dim + DimName,
+                            DefaultAllocator: Allocator<f32, R, C> {
+    pub terminals: Matrix<bool, R, C, DefaultAllocator>,
+    pub values: MatrixMN<f32, R, C>,
+    pub mutables: Matrix<bool, R, C, DefaultAllocator>,
+    pub traversables: Matrix<bool, R, C, DefaultAllocator>,
 }
 
-impl <R, C> Grid<R, C> where R: Dim, C: Dim {
-    pub fn constrain(&self) -> Matrix<f32, R, C, VecStorage<f32, R, C>> {
-        let mut constrained = self.values.clone();
-        constrained
+impl<R, C> Grid<R, C> where R: Dim + DimName, C: Dim + DimName,
+                            DefaultAllocator: Allocator<f32, R, C> {
+    pub fn constrain(&self) -> MatrixMN<f32, R, C> {
+        unimplemented!()
     }
 }
 
-fn main() {
-
-}
+fn main() {}
